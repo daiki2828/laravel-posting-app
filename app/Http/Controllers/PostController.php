1fce1d2f3ee5e729f7d0eaf291b5942e0d15dd36
daiki2828 <barce.flump0321@gmail.com>
2023-03-13 01:04:30 +0900

    index page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PostController extends Controller
-{
+class PostController extends Controller{
     //
+    public function index() {
+        return view('posts.index');
+    }
 }
