a4c07a4e3f37d0a7dfc60c8295e2b83cdf6ae5cb
daiki2828 <barce.flump0321@gmail.com>
2023-04-10 22:24:22 +0900

    index

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// ルーティングを設定するコントローラを宣言する
+use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 投稿の一覧ページ
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+
